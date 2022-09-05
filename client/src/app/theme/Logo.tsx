55785227272463c869eda05046ce6b480693f144
Milosz <57958312+rbnsnn@users.noreply.github.com>
2022-09-05 22:22:59 +0200

    @server - add jwt authorization

---
+++ client/src/app/theme/Logo.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import appLogo from '../../assets/logo.png'
+
+const Logo: React.FC = () => {
+
+    return (
+        <div>
+            <img src={appLogo} alt='TaskTalk Logo' />
+        </div>
+    )
+}
+
+export default Logo
---
+++ client/src/app/theme/theme.ts
@@ -0,0 +1,11 @@
+import { createTheme } from "@mui/material"
+
+export const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#1AB79D',
+      dark: '#0B8A72',
+      contrastText: '#fff'
+    },
+  }
+})
