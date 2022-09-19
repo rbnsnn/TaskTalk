fa1520d74cd509790bd0d6f08670b98daf6574e3
Milosz <57958312+rbnsnn@users.noreply.github.com>
2022-09-19 23:01:53 +0200

    add roles guard and decorator, some minor fixes

--- server/src/users/users.module.ts
+++ server/src/users/users.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose'
+import { RolesGuard } from './guards/roles.guard';
 import { User, UserSchema } from './schemas/user.schema';
 import { UsersService } from './users.service';
 
