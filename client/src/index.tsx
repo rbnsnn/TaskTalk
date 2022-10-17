ccf0f1535f70f4adb13d275b76be79207bee468f
Milosz <milosz.mokrzycki21@gmail.com>
2022-10-17 19:55:30 +0200

    add dnd provider

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -4,18 +4,22 @@ import { store } from './store/store'
 import { Provider } from 'react-redux'
 import { ThemeProvider } from '@emotion/react'
 import { theme } from './theme/theme'
+import { HTML5Backend } from 'react-dnd-html5-backend'
+import { DndProvider } from 'react-dnd'
 
 import App from './app/App'
 
 const container = document.getElementById('root')!
-const root = createRoot(container);
+const root = createRoot(container)
 
 root.render(
     <Provider store={store}>
         <BrowserRouter>
             <ThemeProvider theme={theme}>
-                <App />
+                <DndProvider backend={HTML5Backend}>
+                    <App />
+                </DndProvider>
             </ThemeProvider>
         </BrowserRouter>
     </Provider>
-);
+)
