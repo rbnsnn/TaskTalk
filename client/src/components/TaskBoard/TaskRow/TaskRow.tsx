dd1b800f0cd70c111fd0022a43aae78b3009067d
Milosz <milosz.mokrzycki21@gmail.com>
2022-10-20 21:02:12 +0200

    add task row

--- client/src/components/TaskBoard/TaskRow/TaskRow.tsx
+++ client/src/components/TaskBoard/TaskRow/TaskRow.tsx
@@ -1,7 +1,19 @@
+import { Paper, Typography } from '@mui/material'
+import { maxWidth } from '@mui/system'
 import React from 'react'
 
 const TaskRow = () => {
-    return <div>TaskRow</div>
+    return (
+        <Paper
+            elevation={3}
+            sx={{
+                mt: 2,
+                height: '10%',
+            }}
+        >
+            <Typography>test</Typography>
+        </Paper>
+    )
 }
 
 export default TaskRow
