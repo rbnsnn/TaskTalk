e09fcdf4549a0cb698b48ba1d39e6884daa8ec6d
Miłosz Mokrzycki <milosz.mokrzycki21@gmail.com>
2022-10-21 12:44:58 +0200

    add draging to task

--- client/src/components/TaskBoard/TaskRow/TaskRow.tsx
+++ client/src/components/TaskBoard/TaskRow/TaskRow.tsx
@@ -1,17 +1,33 @@
-import { Paper, Typography } from '@mui/material'
-import { maxWidth } from '@mui/system'
 import React from 'react'
+import { IconButton, Paper, Typography } from '@mui/material'
+import DragIndicatorIcon from '@mui/icons-material/DragIndicator'
+import { useDrag } from 'react-dnd'
 
-const TaskRow = () => {
+const TaskRow: React.FC = () => {
+    const [{ isDragging }, drag, preview] = useDrag(
+        () => ({
+            type: 'task',
+            collect: (monitor) => ({
+                isDragging: !!monitor.isDragging(),
+            }),
+        }),
+        []
+    )
     return (
         <Paper
+            ref={preview}
             elevation={3}
             sx={{
                 mt: 2,
                 height: '10%',
+                opacity: isDragging ? 0.5 : 1,
+                cursor: 'hand',
             }}
         >
             <Typography>test</Typography>
+            <IconButton ref={drag}>
+                <DragIndicatorIcon />
+            </IconButton>
         </Paper>
     )
 }
