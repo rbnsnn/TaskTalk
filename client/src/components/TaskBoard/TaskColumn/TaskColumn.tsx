643c818306d183096d4c1a39c22f52161933a7e7
Milosz <milosz.mokrzycki21@gmail.com>
2022-10-22 08:53:46 +0200

    begin implementing dnd columns

--- client/src/components/TaskBoard/TaskColumn/TaskColumn.tsx
+++ client/src/components/TaskBoard/TaskColumn/TaskColumn.tsx
@@ -1,17 +1,45 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Card, CardContent, Divider } from '@mui/material'
 import ColumnTitle from './ColumnTitle'
 import TaskRow from '../TaskRow/TaskRow'
 
+const taskData = [
+    {
+        id: '1',
+        name: 'test',
+        tasks: [
+            {
+                id: 'a',
+                title: 'chuj',
+            },
+            {
+                id: 'b',
+                title: 'cipa',
+            },
+        ],
+    },
+    {
+        id: '2',
+        name: 'test',
+        tasks: [
+            {
+                id: 'c',
+                title: 'dupa',
+            },
+        ],
+    },
+]
+
 const TaskColumn: React.FC = () => {
+    const [data, setData] = useState(taskData)
     return (
         <Card>
             <ColumnTitle />
             <Divider />
             <CardContent>
-                <TaskRow />
-                <TaskRow />
-                <TaskRow />
+                {data.map((column) => (
+                    <TaskRow />
+                ))}
             </CardContent>
         </Card>
     )
