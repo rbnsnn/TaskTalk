c436fe424f8cbd3f7c4552a8e4d205bf694cb6ea
Milosz <milosz.mokrzycki21@gmail.com>
2022-10-26 20:46:32 +0200

    add user table pagination

--- client/src/components/Users/UsersTable.tsx
+++ client/src/components/Users/UsersTable.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import {
     Paper,
     Table,
@@ -7,6 +7,7 @@ import {
     TableRow,
     TableHead,
     TableBody,
+    TablePagination,
 } from '@mui/material'
 import UserRow from './UserRow'
 import UsersTableTittle from './UsersTableTittle'
@@ -19,36 +20,64 @@ interface Props {
 }
 
 const UsersTable: React.FC<Props> = ({ data, handleOpen, handleUpdate }) => {
+    const [rowsPerPage, setRowsPerPage] = React.useState(5)
+    const [page, setPage] = useState(0)
+
+    const handleChangePage = (event: unknown, newPage: number) => {
+        setPage(newPage)
+    }
+
+    const handleChangeRowsPerPage = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setRowsPerPage(+event.target.value)
+        setPage(0)
+    }
+
     return (
-        <TableContainer
-            component={Paper}
-            sx={{
-                maxWidth: { xs: '95%', sm: '100%' },
-            }}
-        >
-            <UsersTableTittle handleOpen={handleOpen} />
-            <Table size='small'>
-                <TableHead>
-                    <TableRow>
-                        <TableCell variant='headerMain' />
-                        <TableCell variant='headerMain'>Username</TableCell>
-                        <TableCell variant='headerMain'>User ID</TableCell>
-                        <TableCell variant='headerMain'>Role</TableCell>
-                        <TableCell variant='headerMain'>Email</TableCell>
-                        <TableCell variant='headerMain'>Tasks</TableCell>
-                    </TableRow>
-                </TableHead>
-                <TableBody>
-                    {data.map((user: UserData) => (
-                        <UserRow
-                            key={user.username}
-                            user={user}
-                            handleUpdate={handleUpdate}
-                        />
-                    ))}
-                </TableBody>
-            </Table>
-        </TableContainer>
+        <Paper sx={{ width: '100%' }}>
+            <TableContainer
+                component={Paper}
+                sx={{
+                    maxWidth: { xs: '95%', sm: '100%' },
+                }}
+            >
+                <UsersTableTittle handleOpen={handleOpen} />
+                <Table
+                    size='small'
+                    stickyHeader
+                >
+                    <TableHead>
+                        <TableRow>
+                            <TableCell variant='headerMain' />
+                            <TableCell variant='headerMain'>Username</TableCell>
+                            <TableCell variant='headerMain'>User ID</TableCell>
+                            <TableCell variant='headerMain'>Role</TableCell>
+                            <TableCell variant='headerMain'>Email</TableCell>
+                            <TableCell variant='headerMain'>Tasks</TableCell>
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        {data
+                            .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+                            .map((user: UserData) => (
+                                <UserRow
+                                    key={user.username}
+                                    user={user}
+                                    handleUpdate={handleUpdate}
+                                />
+                            ))}
+                    </TableBody>
+                </Table>
+            </TableContainer>
+            <TablePagination
+                rowsPerPageOptions={[5, 10, 25]}
+                component='div'
+                count={data.length}
+                rowsPerPage={rowsPerPage}
+                page={page}
+                onPageChange={handleChangePage}
+                onRowsPerPageChange={handleChangeRowsPerPage}
+            />
+        </Paper>
     )
 }
 
