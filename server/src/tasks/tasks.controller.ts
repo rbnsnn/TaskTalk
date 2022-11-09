c7f2b1dfa372b15de216eb491056db38d80e6541
Milosz <milosz.mokrzycki21@gmail.com>
2022-11-09 22:09:42 +0100

    add task service

--- server/src/tasks/tasks.controller.ts
+++ server/src/tasks/tasks.controller.ts
@@ -1,4 +1,12 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Request } from '@nestjs/common';
+import { TasksService } from './tasks.service';
 
 @Controller('tasks')
-export class TasksController {}
+export class TasksController {
+    constructor(private readonly tasksService: TasksService){}
+
+    @Get()
+    async getAllTasks(@Request() req) {
+        return this.tasksService.getAllTasks(req.company)
+    }
+}
--- server/src/tasks/tasks.service.ts
+++ server/src/tasks/tasks.service.ts
@@ -1,4 +1,13 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable } from '@nestjs/common'
+import { InjectModel } from '@nestjs/mongoose'
+import { Task, TaskDocument } from './schemas/task.schema'
+import { Model } from 'mongoose'
 
 @Injectable()
-export class TasksService {}
+export class TasksService {
+    constructor(@InjectModel(Task.name) private taskModel: Model<TaskDocument>) {}
+    async getAllTasks(company: string) {
+        const tasks = await this.taskModel.find({})
+        return ''
+    }
+}
