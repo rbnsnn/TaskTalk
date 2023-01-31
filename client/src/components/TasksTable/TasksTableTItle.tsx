f351061c434cfff77c43a48e22ba7d7f196493cc
Miłosz Mokrzycki <milosz.mokrzycki21@gmail.com>
2023-01-31 23:01:33 +0100

    add search field to task table

--- client/src/components/TasksTable/TasksTableTItle.tsx
+++ client/src/components/TasksTable/TasksTableTItle.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { Button, Typography, Box } from '@mui/material'
 import { useAppSelector } from '../../hooks/redux-hooks'
 import { RootState } from '../../store/store'
+import TasksTableSearch from './TasksTableSearch'
 
 interface Props {
     handleOpen: () => void
@@ -23,14 +24,21 @@ const TasksTableTitle: React.FC<Props> = ({ handleOpen }) => {
             >
                 {companyName} tasks
             </Typography>
-
-            <Button
-                variant='contained'
-                size='large'
-                onClick={handleOpen}
+            <Box
+                display='flex'
+                flexDirection='row'
+                justifyContent='space-between'
             >
-                Add task
-            </Button>
+                <TasksTableSearch />
+
+                <Button
+                    variant='contained'
+                    size='large'
+                    onClick={handleOpen}
+                >
+                    Add task
+                </Button>
+            </Box>
         </Box>
     )
 }
